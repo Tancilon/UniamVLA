19e004b81ad4ffb8224474e00bd58c67b273f5c9
tancilon <tancilon1@gmail.com>
2026-06-05 21:17:21 -0400

    Add CALVIN ABC-D dataset mixture

---
+++ examples/calvin/train_files/data_registry/data_config.py
@@ -0,0 +1,7 @@
+"""CALVIN dataset mixtures for StarVLA LeRobot training."""
+
+DATASET_NAMED_MIXTURES = {
+    "calvin_abc_d": [
+        ("calvin_abc_d_lerobot_v2.1", 1.0, "libero_franka"),
+    ],
+}
